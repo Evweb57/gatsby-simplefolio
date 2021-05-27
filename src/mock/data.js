f3b9c36df0034988714ad3d60b132443f60469f9
Eric Van Meter <ericvanmeter57@gmail.com>
2021-05-27 16:40:28 -0500

    finished migration

--- src/mock/data.js
+++ src/mock/data.js
@@ -9,57 +9,50 @@ export const headData = {
 
 // HERO DATA
 export const heroData = {
-  title: '',
-  name: '',
-  subtitle: '',
-  cta: '',
+  title: 'Hello, my name is ',
+  name: 'Eric Van Meter',
+  subtitle: `I'm a Full-Stack Javascript Developer`,
+  cta: 'Know more',
 };
 
 // ABOUT DATA
 export const aboutData = {
   img: 'profile.jpg',
-  paragraphOne: '',
-  paragraphTwo: '',
-  paragraphThree: '',
-  resume: 'https://www.resumemaker.online/es.php', // if no resume, the button will not show up
+  paragraphOne:
+    'I design solution architectures, deconstruct problems, and optimize systems as a Technophile who is passionate about technology.',
+  paragraphTwo:
+    'I am a technically savvy, customer-focused Full-stack developer who is enthusiastic about working in the software engineering sector in full-stack web development, demonstrating value to both customers and employers by leveraging relevant tools to solve complex problems through productive collaboration with technical individuals to produce excellent quality products and implementations while focusing on code reusability, creativity and flexibility.',
+  paragraphThree: 'For an updated resume reach out and connect with me on LinkedIn below.',
+  resume: 'https://www.linkedin.com/in/ericdvanmeter/', // if no resume, the button will not show up
 };
 
 // PROJECTS DATA
 export const projectsData = [
   {
     id: nanoid(),
-    img: 'project.jpg',
-    title: '',
-    info: '',
-    info2: '',
-    url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+    img: 'project1.png',
+    title: 'Marshmellow Trail',
+    info: 'Marshmellow trail is a project exploring a bare-bones JavaScript programming style using the standard MVC architecture.',
+    info2: 'MongodDB, MongoAtlas, Express.js, Node.js, and Mapbox.',
+    url: 'https://damp-spire-84324.herokuapp.com/',
+    repo: 'https://github.com/Evweb57/toasty_marshmellows', // if no repo, the button will not show up
   },
   {
     id: nanoid(),
-    img: 'project.jpg',
-    title: '',
-    info: '',
-    info2: '',
-    url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
-  },
-  {
-    id: nanoid(),
-    img: 'project.jpg',
-    title: '',
-    info: '',
-    info2: '',
-    url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+    img: 'project2.png',
+    title: 'Client-Side',
+    info: 'Client-Side explored server side rendering using Next.js and deploying through Zeit.',
+    info2: 'React, MongoDB, Express.js, Node.js, and JavaScript.',
+    url: 'https://project-tracker-ochre.vercel.app/',
+    repo: 'https://github.com/Evweb57/project_tracker', // if no repo, the button will not show up
   },
 ];
 
 // CONTACT DATA
 export const contactData = {
-  cta: '',
-  btn: '',
-  email: '',
+  cta: 'Looking for an enthusiastic developer?',
+  btn: 'Open Email',
+  email: 'ericvanmeter57@gmail.com',
 };
 
 // FOOTER DATA
@@ -67,28 +60,18 @@ export const footerData = {
   networks: [
     {
       id: nanoid(),
-      name: 'twitter',
-      url: '',
-    },
-    {
-      id: nanoid(),
-      name: 'codepen',
-      url: '',
-    },
-    {
-      id: nanoid(),
       name: 'linkedin',
-      url: '',
+      url: 'https://www.linkedin.com/in/ericdvanmeter/',
     },
     {
       id: nanoid(),
       name: 'github',
-      url: '',
+      url: 'https://github.com/Evweb57',
     },
   ],
 };
 
 // Github start/fork buttons
 export const githubButtons = {
-  isEnabled: true, // set to false to disable the GitHub stars/fork buttons
+  isEnabled: false, // set to false to disable the GitHub stars/fork buttons
 };
